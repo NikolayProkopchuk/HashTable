629bf4db90f6fd6365c10ce4202f3657cf32c481
Nikolay <nikolyand@gmail.com>
2022-05-13 12:54:45 +0300

    fixed java docs

--- src/com/prokopchuk/hashtable/HashTable.java
+++ src/com/prokopchuk/hashtable/HashTable.java
@@ -2,6 +2,20 @@ package com.prokopchuk.hashtable;
 
 import java.util.Objects;
 
+/**
+ * A simple implementation of the Hash Table that allows storing a generic key-value pair. The table itself is based
+ * on the array of {@link Node} objects.
+ * <p>
+ * An initial array capacity is 16.
+ * <p>
+ * Every time a number of elements is equal to the array size that tables gets resized
+ * (it gets replaced with a new array that it twice bigger than before). E.g. resize operation will replace array
+ * of size 16 with a new array of size 32. PLEASE NOTE that all elements should be reinserted to the new table to make
+ * sure that they are still accessible  from the outside by the same key.
+ *
+ * @param <K> key type parameter
+ * @param <V> value type parameter
+ */
 public class HashTable<K, V> {
 
     public static void main(String[] args) {
@@ -31,11 +45,13 @@ public class HashTable<K, V> {
     private Node<K, V>[] buckets = new Node[numberOfBuckets];
 
     /**
-     * Adds an element to the hash table. Does not support duplicate elements.
+     * Puts a new element to the table by its key. If there is an existing element by such key then it gets replaced
+     * with a new one, and the old value is returned from the method. If there is no such key then it gets added and
+     * null value is returned.
      *
-     * @param key
-     * @param value
-     * @return true if it was added
+     * @param key   element key
+     * @param value element value
+     * @return old value or null
      */
     public V put(K key, V value) {
         Objects.requireNonNull(key);
@@ -91,10 +107,10 @@ public class HashTable<K, V> {
     }
 
     /**
-     * Prints a hash table according to the following format
-     * 0: Andrii -> Taras
-     * 1: Start
-     * 2: Serhii
+     * Returns a string representation o  table (array) content according to the following format:
+     * 0: key1:value1 -> key2:value2
+     * 1:
+     * 2: key3:value3
      * ...
      */
     public String tableToString() {
